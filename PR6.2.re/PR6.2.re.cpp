dc4b71cb5a65e17de05c1367d3540f4488d9fe6a
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-11-02 10:14:08 +0200

    pr6.2.re

---
+++ PR6.2.re/PR6.2.re.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+void printArray(const int a[], int n) {
+    for (int i = 0; i < n; ++i) {
+        cout << a[i] << ' ';
+    }
+    cout << endl;
+}
+int maxEvenIndex(int a[], int n, int i, int index) {
+    if (i == n) {
+        return index;
+    }
+
+    if (a[i] % 2 == 0 && (index == -1 || a[i] > a[index])) {
+        index = i;
+    }
+    return maxEvenIndex(a, n, i + 1, index);
+}
+
+int main() {
+    int n = 8;
+    int a[8] = { 1, 6, 7, 4, 3, 9, 2, 10 };
+    printArray(a, n);
+    int index = maxEvenIndex(a, n, 0, -1);
+    cout << "maxEvenIndex = " << index << endl;
+    
+    return 0;
+}
---
+++ UnitTest6.2.re/UnitTest6.2.re.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR6.2.re/PR6.2.re.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest62re
+{
+	TEST_CLASS(UnitTest62re)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int n = 6;
+			int a[6] = { 2,5,4,3,6,7 };
+			int index = maxEvenIndex(a,n,0,-1);
+			Assert::AreEqual(4, index);
+		}
+	};
+}
